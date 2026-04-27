4f4858c86087a7915516891a9cf1efb082b1ed19
HellPing <maddin.maddin@yahoo.com>
2026-04-27 16:51:06 +0200

    Removed expensive dom querys on every message

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -237,7 +237,6 @@ function mountObserver(container: HTMLElement) {
     mutations.forEach((mutation) => {
       mutation.addedNodes.forEach((node) => {
         if (!(node instanceof HTMLElement)) return;
-        if (node.querySelector(".palsona-icon")) return;
         processNode(node);
       });
     });
@@ -342,16 +341,9 @@ function disconnectObserver() {
  * @param node The chat message element.
  * @returns The username element.
  */
-function getUsernameElement(node: HTMLElement): HTMLElement | undefined {
-  // select any elements where the class contains the word "username" or "author"
-  // this is most likely the element the username is in, regardless of other installed addons
-  const usernameElement = node.querySelector<HTMLElement>('[class*="username"], [class*="author"]');
-  if (!usernameElement) return;
-  // check if there's another username element inside the detected element
-  // on native this is important to select only the name and not the element containing badges + name
-  const innerUsernameEl = usernameElement.querySelector<HTMLElement>('[class*="username"]');
-
-  return innerUsernameEl || usernameElement;
+function getUsernameElement(node: HTMLElement): HTMLElement | null {
+  const usernameElement = node.querySelector<HTMLElement>(".seventv-chat-user-username, .chat-line__username, .video-chat__message-author");
+  return usernameElement;
 }
 
 /**
@@ -372,9 +364,6 @@ function handleUsernameLocalization(username: string): string {
  */
 function processNode(node: Node) {
   if (!(node instanceof HTMLElement)) return;
-
-  // palsona-icon already appended
-  if (node.querySelector<HTMLElement>(".palsona-icon")) return;
 
   // get username
   const usernameElement = getUsernameElement(node);
